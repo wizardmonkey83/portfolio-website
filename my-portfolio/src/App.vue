44f5d9a959a8c2448ea21a38de2bb74f0eb436c5
brompley <brompley@mac.myfiosgateway.com>
2026-04-06 18:02:08 -0400

    working on the profile pocket component

--- my-portfolio/src/App.vue
+++ my-portfolio/src/App.vue
@@ -1,7 +1,8 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import ProfilePocket from './components/ProfilePocket.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <ProfilePocket name="Benjamin Brady" title="Software Engineer"  />
 </template>
---
+++ my-portfolio/src/components/ProfilePocket.vue
@@ -0,0 +1,53 @@
+<script setup>
+    import vueLogo from '../assets/vue.svg'
+
+    const props = defineProps({
+        name: String,
+        title: String,
+        description: String
+    })
+</script>
+
+<template>
+    <section class="profile">
+        <section class="profile-left">
+            <div class="profile-img-container">
+                <img :src="vueLogo" class="profile-img"/>
+            </div>
+            <div class="profile-left-text">
+                <h1 class="header name">{{  props.name }}</h1>
+                <h1 class="header title">{{  props.title }}</h1>
+            </div>
+        </section>
+
+        <section class="profile-right">
+
+        </section>
+    </section>
+</template>
+
+
+<style scoped>
+.profile {
+    display: flexbox;
+    flex-direction: row;
+    justify-content: center;
+    align-items: flex-start;
+}
+
+.profile-left {
+    display: flexbox;
+    flex-direction: column;
+}
+
+.profile-left-text { 
+    display: flexbox;
+    justify-content: flex-start;
+    align-items: flex-start;
+}
+
+.profile-img {
+    border-radius: 50%;
+}
+</style>
+
